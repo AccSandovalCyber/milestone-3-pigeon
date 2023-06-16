968d14d89efeab9846089e024bbfedb41f3c3292
Ricardo Sandovale <AccSandoval13@gmail.comm>
2023-06-15 22:56:30 -0700

    HomePage.Js Routes Natalie

--- src/firebase.js
+++ src/firebase.js
@@ -9,7 +9,7 @@ apiKey: "AIzaSyAAIDgfLMGijRneFKC6l9q6pClqm8b7M7o",
 authDomain: "milestone-p3-pigeon.firebaseapp.com",
 databaseURL: "https://milestone-p3-pigeon-default-rtdb.firebaseio.com",
 projectId: "milestone-p3-pigeon",
-storageBucket: "milestone-p3-pigeon.appspot.com",
+storageBucket: "https://milestone-p3-pigeon-default-rtdb.firebaseio.com",
 messagingSenderId: "2997406736",
 appId: "1:2997406736:web:8e793e724dfc9b96cf4e95",
 measurementId: "G-HRX5LPX9BN"
