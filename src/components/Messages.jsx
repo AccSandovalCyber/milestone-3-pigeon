0686663eff8d9f0b1e63937192f8fee09ba712e0
Ricardo Sandovale <AccSandoval13@gmail.comm>
2023-06-17 14:20:32 -0700

    Removed WebVitals

--- src/components/Messages.jsx
+++ src/components/Messages.jsx
@@ -1,13 +1,13 @@
-import Firebase
+// import Firebase
 
-const messaging = getMessaging(app);
+// const messaging = getMessaging(app);
 
-const messaging = getMessaging();
-// Add the public key generated from the console here.
-getToken(messaging, {vapidKey: "BOeqcLnENQwlC6YVY-t0eYYoa0rr78PNAWmjINTzKWtYXsjKKxoPv4CXKBDVZQhWkaTO1GKuYEvYPEvG_NF1RHI"});
+// const messaging = getMessaging();
+// // Add the public key generated from the console here.
+// getToken(messaging, {vapidKey: "BOeqcLnENQwlC6YVY-t0eYYoa0rr78PNAWmjINTzKWtYXsjKKxoPv4CXKBDVZQhWkaTO1GKuYEvYPEvG_NF1RHI"});
 
-function requestPermission() {
-    console.log('Requesting permission...');
-    Notification.requestPermission().then((permission) => {
-      if (permission === 'granted') {
-        console.log('Notification permission granted.');
+// function requestPermission() {
+//     console.log('Requesting permission...');
+//     Notification.requestPermission().then((permission) => {
+//       if (permission === 'granted') {
+//         console.log('Notification permission granted.');
