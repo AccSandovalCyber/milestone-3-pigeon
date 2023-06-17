7ba51a771d7581d90c78d00b0919ca267a5f5a86
Natalievo1 <natalieojeda@icloud.com>
2023-06-17 14:16:49 -0700

    adding messaging behavior

---
+++ src/components/Messages.jsx
@@ -0,0 +1,13 @@
+import Firebase
+
+const messaging = getMessaging(app);
+
+const messaging = getMessaging();
+// Add the public key generated from the console here.
+getToken(messaging, {vapidKey: "BOeqcLnENQwlC6YVY-t0eYYoa0rr78PNAWmjINTzKWtYXsjKKxoPv4CXKBDVZQhWkaTO1GKuYEvYPEvG_NF1RHI"});
+
+function requestPermission() {
+    console.log('Requesting permission...');
+    Notification.requestPermission().then((permission) => {
+      if (permission === 'granted') {
+        console.log('Notification permission granted.');
