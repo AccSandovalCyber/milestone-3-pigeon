da3f31c6b7640bacf051a7c339ac99f9df44aa78
Anthony Iniguez <aginiguez33@gmail.com>
2023-06-18 04:00:24 -0700

    added notes to home and chatbox for natalie and stephen

--- src/components/Chatbox.jsx
+++ src/components/Chatbox.jsx
@@ -4,6 +4,8 @@ const ChatBox = ({ user }) => {
     let content = null;
   
     if (user) {
+    //within conetent is where Stephen and Natalie will add their chat box code
+    //content renders if the user is in true.
       content = (
         <div>
           <h3>This is where the chat and messages will render {user.displayName}</h3>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Chatbox from '../components/Chatbox';
 
+//chatbox is render using a ternary operator which is a conditonal statement. User prop is true/false
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Conditional_operator
 const Home = ({ user }) => {
   return (
     <div className='home'>
       <div className="container">
-        {user ? <Chatbox user={user} /> : 'login'}
+        {user ? <Chatbox user={user} /> : 'login'} 
       </div>
     </div>
   );
