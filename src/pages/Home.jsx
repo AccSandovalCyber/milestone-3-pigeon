12b5db242419827eb899fe3b7c776c251171cf7c
pak-s <steeevepak@gmail.com>
2023-06-19 00:30:22 -0700

    styling changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Chatbox from '../components/Chatbox';
 
+
 //chatbox is render using a ternary operator which is a conditonal statement. User prop is true/false
 //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Conditional_operator
 const Home = ({ user }) => {
