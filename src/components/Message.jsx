b14e60c6e8f3e83b43c37d4a80aa4a7d436ba82e
Natalievo1 <natalieojeda@icloud.com>
2023-06-20 00:52:24 -0700

    adding pigeon

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -10,7 +10,7 @@ const Message = ({ message }) => {
       className={`chat-bubble ${message.uid === user.uid ? "right" : ""}`}>
       <img
         className="chat-bubble__left"
-        src={message.avatar}
+        src="https://cdn.discordapp.com/attachments/1117664004639301655/1120617425914822676/image.png"
         alt="user avatar"
       />
       <div className="chat-bubble__right">
