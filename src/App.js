c7dcde8d5927562f9ec43e333146c75ffe514a40
Oscar Lopez <113158187+LopEm15@users.noreply.github.com>
2023-06-19 21:51:04 -0700

    Debugging

--- src/App.js
+++ src/App.js
@@ -3,22 +3,14 @@ import Home from './pages/Home';
 // import CreateUser from './pages/CreateUser';
 import './style.scss';
 import { BrowserRouter } from 'react-router-dom';
-import Navbar from './components/Navbar';
+// import Navbar from './components/Navbar';
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
         <div className="header">
-<<<<<<< HEAD
-          <Navbar />
-          <Login />
-<<<<<<< HEAD
-=======
-=======
->>>>>>> 5aa942c53a57ec37bbde0809847ba806fe4105d1
           <Home /> 
->>>>>>> 2333d717478f44d4ab55c95acb2118c5a5368ecf
         </div>
       </div>
      </BrowserRouter>
@@ -26,5 +18,3 @@ function App() {
 }
 
 export default App;
-
-
