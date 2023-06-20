3c1de25e9520df77de9f32cd05dbd87197aa0221
Natalievo1 <natalieojeda@icloud.com>
2023-06-20 10:30:50 -0700

    fixed nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -17,21 +17,16 @@ const NavBar = () => {
 
   // Our NavBar component allows a user to sign in with Google or sign out of the app 
   return (
-<<<<<<< HEAD
     <nav className="nav-bar">
       <h1>Pigeon</h1>
-=======
-    <nav className="nav-bar"> 
-      <h1>React Chat</h1>
->>>>>>> 5cd2c4ab8a73020b8a83b6c7f89c2b1ac1fbca2e
       {user ? (
         <button onClick={signOut} className="sign-out" type="button">
           Sign Out
         </button>
       ) : (
-        
+
           <button className='sign-in' onClick={googleSignIn}> Sign In With Google </button>
-      
+
       )}
     </nav>
   );
