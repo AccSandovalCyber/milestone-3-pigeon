0dc0405650b3dfff9039c7cb151214f325fd0d17
Ricardo Sandovale <AccSandoval13@gmail.comm>
2023-06-20 18:34:26 -0700

    Index.Js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,29 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+// Ricardo commented out the code above because it was using React 17
+// I replaced it with the code below which uses React 18, the console error went away 
+
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const root = createRoot(document.getElementById('root'));
+root.render(
+<React.StrictMode>
+  <App />
+</React.StrictMode>,
+  ); 
+
+
+
