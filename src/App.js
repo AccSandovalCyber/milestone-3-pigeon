1e252b3900d93977c21915867af0dd5719131720
Ricardo Sandovale <AccSandoval13@gmail.comm>
2023-06-21 18:25:09 -0700

    Updated Comments

--- src/App.js
+++ src/App.js
@@ -1,23 +1,3 @@
-// import React from 'react';
-// import Home from './pages/Home';
-// // import CreateUser from './pages/CreateUser';
-// import './style.scss';
-// import { BrowserRouter } from 'react-router-dom';
-// // import Navbar from './components/Navbar';
-
-// function App() {
-//   return (
-//     <BrowserRouter>
-//       <div className="App">
-//         <div className="header">
-//           <Home /> 
-//         </div>
-//       </div>
-//      </BrowserRouter>
-//   );
-// }
-
-// export default App;
 import { auth } from "./firebase";
 import { useAuthState } from "react-firebase-hooks/auth";
 import "./style.scss"
--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { auth } from "../firebase";
-import { useAuthState } from "react-firebase-hooks/auth";
+import { useAuthState } from "react-firebase-hooks/auth"; 
 
 const Message = ({ message }) => {
   const [user] = useAuthState(auth);
